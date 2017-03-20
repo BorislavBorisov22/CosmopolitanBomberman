3ac33f493b42dd97f26ce463cb5e6fef332f1b6f
vladi99 <vlpetrovic99@gmail.com>
2017-03-20 22:36:10 +0200

    add checkIfBombermanHitsNonWalkable function and some modification in code appearance

--- Bomberman/JavaScript/Common/movement.js
+++ Bomberman/JavaScript/Common/movement.js
@@ -1,13 +1,6 @@
 function updateBomberManPosition(bomberMan, canvas, dirDeltas, dir) {
-
-    const lastCoordinates = { x: bomberMan.y, y: bomberMan.y };
-
-    console.log(bomberMan);
-
     bomberMan.x = (bomberMan.x + canvas.width) % canvas.width;
     bomberMan.y = (bomberMan.y + canvas.height) % canvas.height;
     bomberMan.x += dirDeltas[dir].x;
     bomberMan.y += dirDeltas[dir].y;
-
-    return lastCoordinates;
 }
