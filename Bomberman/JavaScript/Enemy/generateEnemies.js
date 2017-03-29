92d15f0d2f5a67654b26815bf7522a458ef56e3a
vladi99 <vlpetrovic99@gmail.com>
2017-03-29 17:52:06 +0300

    Fix enemy generation

--- Bomberman/JavaScript/Enemy/generateEnemies.js
+++ Bomberman/JavaScript/Enemy/generateEnemies.js
@@ -5,8 +5,10 @@ function generateEnemies(field, enemiesCount, enemiesContext) {
         const row = (Math.random() * (field.length - 2)) | 0;
         const col = (Math.random() * (field[0].length - 2)) | 0;
 
+        console.log(field);
+
         if (field[row][col] !== WALL_CHAR && field[row][col] !== BRICK_CHAR) {
-            const enemyBody = new PhysicalBody(row * CELL_SIZE, col * CELL_SIZE, (Math.random() * 4) | 0, CELL_SIZE, CELL_SIZE);
+            const enemyBody = new PhysicalBody(col * CELL_SIZE, row * CELL_SIZE, (Math.random() * 4) | 0, CELL_SIZE, CELL_SIZE);
 
             const enemySprite = new Sprite({
                 width: CELL_SIZE,
